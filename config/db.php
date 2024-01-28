706df963639084d0fdd341d605ea0d24e5211f16
Tevinteru <138574634+Tevinteru@users.noreply.github.com>
2024-01-28 17:35:14 +0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,10 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-    //'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    //'username' => 'root',
-    //'password' => '',
-    //'charset' => 'utf8',
+    'dsn' => 'mysql:host=alecksu6.beget.tech;dbname=alecksu6_s',
+    'username' => 'alecksu6_s',
+    'password' => 'Q1qqq!',
+    'charset' => 'utf8',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
